e084d5641b03a0d45563dd6f583622e0eab1dc76
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-09-06 10:28:04 +0530

    Add files via upload

---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+
+export default function notFound (){
+  return (
+    <div className="flex justify-center my-14"><div className="text-3xl">something went wrong  | <sub className="font-bold"><strong>404 </strong> page not found</sub> </div></div>
+  )
+}
+
